a01e7796d6777a4f7e1c4da28c56669a7dca4015
jenisdev <wangnaixu88@gmail.com>
2023-06-13 18:24:23 +0300

    x64 build version

---
+++ Chatgpt13/Chatgpt13.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "ChatGgtAPI.hpp"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	WSADATA w = {};
+	WSAStartup(MAKEWORD(2, 2), &w);
+	CHATGPT_API c("your-api-key");
+	c.SetModel("text-ada-001");
+	for (;;)
+	{
+		std::string input;
+		std::cout << "Enter question:";
+		std::getline(std::cin, input);
+		if (input.empty())
+			break;
+		auto off = c.Text(input.c_str());
+		if (!off.has_value())
+			continue;
+		auto& r = off.value();
+		std::cout << r.t << std::endl;
+	}
+}
