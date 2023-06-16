c3e790e245a16f468bf049a53e3254bcbd1eafd9
jenisdev <wangnaixu88@gmail.com>
2023-06-16 04:02:23 +0300

    cleaning up the code

--- Chatgpt13/ChatGgtAPI.hpp
+++ Chatgpt13/ChatGgtAPI.hpp
@@ -12,34 +12,42 @@
 #include "rest.h"
 #include "boost\optional.hpp"
 
+#define _BUFFERSIZE 10000
+#define UNSIGLONG unsigned long
+#define STD_CHARVECTOR std::vector<char>
+#define STDSTR std::string
+#define JSONOBJECT jsonxx::Object
+#define JSONARRAY jsonxx::Array
+#define JSONSTRING jsonxx::String
+
 struct CHATGPT_RESULT
 {
-	jsonxx::Object o;
-	std::string t;
-	std::vector<char> data;
+	JSONOBJECT o;
+	STDSTR t;
+	STD_CHARVECTOR data;
 };
 
-std::vector<char> Fetch(const char* TheLink)
+STD_CHARVECTOR Fetch(const char* TheLink)
 {
-	std::vector<char> resp;
+	STD_CHARVECTOR resp;
 
 	DWORD Size;
-	unsigned long bfs = 1000;
-	TCHAR ss[1000];
+	UNSIGLONG bufferSize = _BUFFERSIZE;
+	TCHAR ss[_BUFFERSIZE];
 	DWORD TotalTransferred = 0;
 
-	int err = 1;
+	int ERR = 1;
 
-	HINTERNET hI = 0, hRead = 0;
+	HINTERNET hInternet = 0, hRequest = 0;
 
-	hI = InternetOpen(L"ChatGPT-API", INTERNET_OPEN_TYPE_DIRECT, 0, 0, 0);
-	if (hI)
+	hInternet = InternetOpen(L"ChatGPT-API", INTERNET_OPEN_TYPE_DIRECT, 0, 0, 0);
+	if (hInternet)
 	{
-		hRead = InternetOpenUrlA(hI, TheLink, 0, 0, INTERNET_FLAG_NO_CACHE_WRITE, 0);
-		if (hRead)
+		hRequest = InternetOpenUrlA(hInternet, TheLink, 0, 0, INTERNET_FLAG_NO_CACHE_WRITE, 0);
+		if (hRequest)
 		{
 
-			if (!HttpQueryInfo(hRead, HTTP_QUERY_CONTENT_LENGTH, ss, &bfs, 0))
+			if (!HttpQueryInfo(hRequest, HTTP_QUERY_CONTENT_LENGTH, ss, &bufferSize, 0))
 				Size = (DWORD)-1;
 			else
 				Size = _wtoi(ss);
@@ -49,16 +57,16 @@ std::vector<char> Fetch(const char* TheLink)
 				DWORD n;
 				char Buff[100010] = { 0 };
 				memset(Buff, 0, 100010);
-				BOOL  F = InternetReadFile(hRead, Buff, 100000, &n);
+				BOOL  F = InternetReadFile(hRequest, Buff, 100000, &n);
 				if (F == false)
 				{
-					err = 2;
+					ERR = 2;
 					break;
 				}
 				if (n == 0)
 				{
 					// End of file !
-					err = 0;
+					ERR = 0;
 					break;
 				}
 				TotalTransferred += n;
@@ -73,75 +81,79 @@ std::vector<char> Fetch(const char* TheLink)
 				if (Size != -1)
 					NewPos = (100 * TotalTransferred) / Size;
 			}
-			InternetCloseHandle(hRead);
+			InternetCloseHandle(hRequest);
 		}
-		InternetCloseHandle(hI);
+		InternetCloseHandle(hInternet);
 	}
 	return resp;
 }
 
 class CHATGPT_API
 {
-	std::string tok;
-	std::string model = "text-davinci-003";
+	STDSTR APIKEY;
+	STDSTR model;
+
 	public:
-	CHATGPT_API(const char* api_key)
+	CHATGPT_API(const char* KEY)
 	{
-		tok = api_key;
+		APIKEY = KEY;
 	}
 
-	void SetModel(const char* m)
+	void SetModel(const char* MODEL)
 	{
-		model = m;
+		model = MODEL;
 	}
 
 	std::wstring Bearer()
 	{
 		wchar_t auth[200] = {};
-		swprintf_s(auth, 200, L"Authorization: Bearer %S", tok.c_str());
+		swprintf_s(auth, 200, L"Authorization: Bearer %S", APIKEY.c_str());
 		return auth;
 	}
 
-	boost::optional<CHATGPT_RESULT> Text(const char* prompt, int Temperature = 0, int max_tokens = 100)
+	boost::optional<CHATGPT_RESULT> Text(const char* prompt, int Temperature = 0, int max_tokens = 4096)
 	{
-		std::vector<char> data(10000);
-
-		sprintf_s(data.data(), 10000,
+		STD_CHARVECTOR data(_BUFFERSIZE);
+		sprintf_s(data.data(), _BUFFERSIZE,
 			R"({"model": "%s", "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": "%s"}]})",
 			model.c_str(), prompt);
 
 		data.resize(strlen(data.data()));
-		std::string str = data.data();
-		RESTAPI::REST r;
-		r.Connect(L"api.openai.com", true, 0, 0, 0, 0);
+
+		STDSTR str = data.data();
+
+		RESTAPI::REST hREST;
+		hREST.Connect(L"api.openai.com", true, 0, 0, 0, 0);
 		std::initializer_list<std::wstring> hdrs = {
 			Bearer(),
 			L"Content-Type: application/json",
 		};
-		auto hi = r.RequestWithBuffer(L"/v1/chat/completions", L"POST", hdrs, data.data(), data.size());
-		std::vector<char> out;
-		r.ReadToMemory(hi, out);
-		out.resize(out.size() + 1);
+		auto hInternetConnection = hREST.RequestWithBuffer(L"/v1/chat/completions", L"POST", hdrs, data.data(), data.size());
+
+		STD_CHARVECTOR hRESPONSE;
+		hREST.ReadToMemory(hInternetConnection, hRESPONSE);
+		hRESPONSE.resize(hRESPONSE.size() + 1);
+
 		try
 		{
-			jsonxx::Object o;
-			o.parse(out.data());
+			JSONOBJECT jObject;
+			jObject.parse(hRESPONSE.data());
 			CHATGPT_RESULT r;
-			r.o = o;
+			r.o = jObject;
 			
-			if (o.has<jsonxx::Object>("error"))
-				r.t = o.get<jsonxx::Object>("error").get<jsonxx::String>("message");
+			if (jObject.has<JSONOBJECT>("error"))
+				r.t = jObject.get<JSONOBJECT>("error").get<jsonxx::String>("message");
 			else {
-				auto& choices = o.get<jsonxx::Array>("choices");
-				auto& choice0 = choices.get<jsonxx::Object>(0);
-				auto& message = choice0.get<jsonxx::Object>("message");
-				r.t = message.get<jsonxx::String>("content");
+				auto& choices = jObject.get<JSONARRAY>("choices");
+				auto& choice0 = choices.get<JSONOBJECT>(0);
+				auto& message = choice0.get<JSONOBJECT>("message");
+				r.t = message.get<JSONSTRING>("content");
 			}
 			return r;
 		}
-		catch (...)
+		catch (int Err)
 		{
-			
+			std::cout << "An exception occurred. Exception Nr." << Err << std::endl;
 		}
 		return{};
 	}
--- Chatgpt13/Chatgpt13.cpp
+++ Chatgpt13/Chatgpt13.cpp
@@ -5,16 +5,17 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	WSADATA w = {};
 	WSAStartup(MAKEWORD(2, 2), &w);
-	CHATGPT_API c("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");
-	c.SetModel("gpt-3.5-turbo");
+	CHATGPT_API ChatGPT_OBJ("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");
+	ChatGPT_OBJ.SetModel("gpt-3.5-turbo");
 	for (;;)
 	{
-		std::string input;
+		std::string prompt;
 		std::cout << "Enter question:";
-		std::getline(std::cin, input);
-		if (input.empty())
+
+		std::getline(std::cin, prompt);
+		if (prompt.empty())
 			break;
-		auto off = c.Text(input.c_str());
+		auto off = ChatGPT_OBJ.Text(prompt.c_str());
 		if (!off.has_value())
 			continue;
 		auto& r = off.value();
